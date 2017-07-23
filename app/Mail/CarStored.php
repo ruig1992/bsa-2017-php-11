2de2c2b836c17e2d3457f64c7ea11707e1106874
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-23 23:00:01 +0300

    Added CarStored mailable

---
+++ app/Mail/CarStored.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Mail;
+
+use App\Entity\Car;
+use App\Entity\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+/**
+ * Class CarStored
+ * @package App\Mail
+ */
+class CarStored extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var \App\Entity\User
+     */
+    protected $user;
+    /**
+     * @var \App\Entity\Car
+     */
+    protected $car;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Car $car)
+    {
+        $this->user = $user;
+        $this->car = $car;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(): self
+    {
+        return $this->view('emails.cars.stored')
+            ->with([
+                'user' => $this->user->full_name,
+                'car' => $this->car->toArray(),
+            ]);
+    }
+}
