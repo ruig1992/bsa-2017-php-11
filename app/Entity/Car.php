2a67ff5bb8369b4cafb279e3265323adde6efa80
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-23 21:58:30 +0300

    Updated Entities

--- app/Entity/Car.php
+++ app/Entity/Car.php
@@ -1,11 +1,19 @@
 <?php
-
 namespace App\Entity;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Car
+ * @package App\Entity
+ */
 class Car extends Model
 {
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 
     /**
@@ -14,15 +22,27 @@ class Car extends Model
      * @var array
      */
     protected $fillable = [
-        'color',
         'model',
         'registration_number',
         'year',
+        'color',
         'mileage',
         'price',
         'user_id',
     ];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'price' => 'float',
+    ];
+
+    /**
+     * Get the user that owns the car.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Entity/User.php
+++ app/Entity/User.php
@@ -1,13 +1,18 @@
 <?php
-
 namespace App\Entity;
 
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableInterface;
+use App\Traits\Eloquent\HasActive;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableInterface;
 
+/**
+ * Class User
+ * @package App\Entity
+ */
 class User extends Authenticatable implements AuthenticatableInterface
 {
-    public $timestamps = false;
+    use HasActive, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,23 +22,54 @@ class User extends Authenticatable implements AuthenticatableInterface
     protected $fillable = [
         'first_name',
         'last_name',
-        'is_active',
-        'name',
         'email',
-        'password'
+        'password',
+        'is_active',
+        'is_admin',
     ];
 
+    /**
+     * The attributes that should be hidden.
+     *
+     * @var array
+     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
     /**
+     * The attributes that should be cast to native types.
+     *
      * @var array
      */
     protected $casts = [
         'is_active' => 'boolean',
+        'is_admin' => 'boolean',
     ];
 
+    /**
+     * Get the full name of the user.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    /**
+     * Check if the user is an Administrator.
+     *
+     * @return boolean
+     */
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
+    }
+
+    /**
+     * Get the cars for the user.
+     */
     public function cars()
     {
         return $this->hasMany(Car::class);
